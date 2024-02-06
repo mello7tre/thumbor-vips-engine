2fe93be91f92acf41ec14ba7289e5c7928ead0bf
mello7tre <mello+github@ankot.org>
2024-02-06 17:08:54 +0100

    try with gap=0.0

--- thumbor_vips_engine/engine.py
+++ thumbor_vips_engine/engine.py
@@ -72,7 +72,7 @@ class Engine(BaseEngine):  # pylint: disable=too-many-public-methods
         elif height > width:
             scale = float(width) / self.image.width
 
-        self.image = self.image.resize(scale, kernel="linear")
+        self.image = self.image.resize(scale, gap=0.0)
 
     def focus(self, points: List[FocalPoint]) -> None:
         pass
